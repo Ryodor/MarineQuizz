5ae56830429af7933380f3b864a61fc80c61f1c7
kcoquerelle <coquerelle.k@gmail.com>
2018-06-29 11:06:16 +0200

    version finale

--- js/quizz.js
+++ js/quizz.js
@@ -1,41 +1,38 @@
 function question2() {
     document.getElementById("question").innerHTML = `
-    <div id="question" class="q2">
-        <p class="text">Quel groupe est le plus INSUPORTABLE!?</p>
+    <div class="formSection q2">
+        <p class="text boldP">Quel groupe est le plus INSUPORTABLE!?</p>
         <table>
             <form action="">
                 <tr>
 
                     <th>
-                        <img src="../image/cédric_adam.jpg" alt="André De Soussa">
+                        <img id="img1" class="choice" src="../image/cédric_adam.jpg" alt="André De Soussa">
                     </th>
                     <th>
-                        <img src="../image/kiki.jpg" alt="Julien Pavon">
+                        <img id="img2" class="choice" src="../image/kiki.jpg" alt="Julien Pavon">
                     </th>
                     <th>
-                    <img src="../image/isa.jpg" alt="Guillaume Warckol">
+                    <img id="img3" class="choice" src="../image/isa.jpg" alt="Guillaume Warckol">
                     </th>
                 </tr>
                 <tr>
                     <th>
-                        <input type="radio" id="contactChoice1" name="contact" value="André De Sousa">
                         <label for="contactChoice1">Groupe A</label>
                     </th>
 
                     <th>
-                        <input type="radio" id="contactChoice2" name="contact" value="Julien Pavon">
                         <label for="contactChoice2">Groupe B</label>
                     </th>
 
                     <th>
-                        <input type="radio" id="contactChoice3" name="contact" value="Guillaume Warckol">
                         <label for="contactChoice3">Temps Plein</label>
                     </th>
                 </tr>
                 <tr>
                     <th></th>
                     <th>
-                        <button id="nextbutton" onclick="question3()">Next</button>
+                        <div id="nextbutton" class="button" onclick="question3()">Next</div>
                     </th>
                 </tr>
                 <tr>
@@ -48,46 +45,44 @@ function question2() {
         </table>
     </div>
     `;
+    setListeners();
   }
 
   function question3() {
     document.getElementById("question").innerHTML = `
-    <div id="question" class="q3">
-        <p class="text">Qui est la meilleur coordinatrice pédagogique?</p>
+    <div class="formSection q3">
+        <p class="text boldP">Qui est la meilleure coordinatrice pédagogique?</p>
         <table>
             <form action="">
                 <tr>
 
                     <th>
-                        <img src="../image/marine_nationale.jpg" alt="André De Soussa">
+                        <img id="img1" class="choice" src="../image/marine_nationale.jpg" alt="André De Soussa">
                     </th>
                     <th>
-                        <img src="../image/marine_huertas.jpg" alt="Julien Pavon">
+                        <img id="img2" class="choice" src="../image/marine_huertas.jpg" alt="Julien Pavon">
                     </th>
                     <th>
-                        <img src="../image/marinelepencarré.jpeg" alt="André De Soussa">
+                        <img id="img3" class="choice" src="../image/marinelepencarré.jpeg" alt="André De Soussa">
                     </th>
                 </tr>
                 <tr>
                     <th>
-                        <input type="radio" id="contactChoice1" name="contact" value="André De Sousa">
-                        <label for="contactChoice1">Marine National</label>
+                        <label for="contactChoice1">Marine Nationale</label>
                     </th>
 
                     <th>
-                        <input type="radio" id="contactChoice2" name="contact" value="Julien Pavon">
                         <label for="contactChoice2">Marine Huertas</label>
                     </th>
 
                     <th>
-                        <input type="radio" id="contactChoice1" name="contact" value="André De Sousa">
                         <label for="contactChoice1">Marine Le Pen</label>
                     </th>
                 </tr>
                 <tr>
                     <th></th>
                     <th>
-                        <button id="nextbutton" onclick="question4()">Next</button>
+                        <div id="nextbutton" class="button" onclick="question4()">Next</div>
                     </th>
                 </tr>
                 <tr>
@@ -101,46 +96,44 @@ function question2() {
         </table>
     </div>
     `;
+    setListeners();
   }
 
   function question4() {
     document.getElementById("question").innerHTML = `
-    <div id="question" class="q4">
-        <p class="text">Qui était le PO de NoSql ?</p>
+    <div class="formSection q4">
+        <p class="text boldP">Qui était le PO de NoSql ?</p>
         <table>
             <form action="">
                 <tr>
 
                     <th>
-                        <img src="../image/lauris_braka.jpeg" alt="André De Soussa">
+                        <img id="img1" class="choice" src="../image/lauris_braka.jpeg" alt="André De Soussa">
                     </th>
                     <th>
-                        <img src="../image/Damien_r.jpeg" alt="Julien Pavon">
+                        <img id="img2" class="choice" src="../image/Damien_r.jpeg" alt="Julien Pavon">
                     </th>
                     <th>
-                        <img src="../image/sylvain_delafoy.jpeg" alt="Guillaume Warckol">
+                        <img id="img3" class="choice" src="../image/sylvain_delafoy.jpeg" alt="Guillaume Warckol">
                     </th>
                 </tr>
                 <tr>
                     <th>
-                        <input type="radio" id="contactChoice1" name="contact" value="André De Sousa">
                         <label for="contactChoice1">Lauris Braka</label>
                     </th>
 
                     <th>
-                        <input type="radio" id="contactChoice2" name="contact" value="Julien Pavon">
                         <label for="contactChoice2">Damien Russier</label>
                     </th>
 
                     <th>
-                        <input type="radio" id="contactChoice3" name="contact" value="Guillaume Warckol">
                         <label for="contactChoice3">Sylvain Delafoy</label>
                     </th>
                 </tr>
                 <tr>
                     <th></th>
                     <th>
-                        <button id="nextbutton" onclick="question5()">Next</button>
+                        <div id="nextbutton" class="button" onclick="question5()">Next</div>
                     </th>
                 </tr>
                 <tr>
@@ -154,46 +147,44 @@ function question2() {
         </table>
     </div>
     `;
+    setListeners();
   }
 
   function question5() {
     document.getElementById("question").innerHTML = `
-  <div id="question" class="q1">
-  <p class="text">Où sont les cables HDMI ?</p>
+  <div class="formSection q1">
+  <p class="text boldP">Où sont les adaptateurs HDMI ?</p>
   <table>
       <form action="">
           <tr>
 
               <th>
-                  <img src="../image/tiroir_coding.jpg" alt="André De Soussa">
+                  <img id="img1" class="choice" src="../image/tiroir_coding.jpg" alt="André De Soussa">
               </th>
               <th>
-                  <img src="../image/tv_magouille.jpg" alt="Julien Pavon">
+                  <img id="img2" class="choice" src="../image/tv_magouille.jpg" alt="Julien Pavon">
               </th>
               <th>
-                  <img src="../image/erreur-404-0.jpg" alt="Guillaume Warckol">
+                  <img id="img3" class="choice" src="../image/erreur-404-0.jpg" alt="Guillaume Warckol">
               </th>
           </tr>
           <tr>
               <th>
-                  <input type="radio" id="contactChoice1" name="contact" value="André De Sousa">
-                  <label for="contactChoice1">Bien rangé dans les tiroirs à leurs place ?</label>
+                  <label for="contactChoice1">Bien rangés dans les tiroirs à leur place ?</label>
               </th>
 
               <th>
-                  <input type="radio" id="contactChoice2" name="contact" value="Julien Pavon">
-                  <label for="contactChoice2">Vous pouvez répéter la question.</label>
+                  <label for="contactChoice2">Vous pouvez répéter la question ?</label>
               </th>
 
               <th>
-                  <input type="radio" id="contactChoice3" name="contact" value="Guillaume Warckol">
-                  <label for="contactChoice3">Ont les cherches encore...</label>
+                  <label for="contactChoice3">On les cherche encore...</label>
               </th>
           </tr>
           <tr>
               <th></th>
               <th>
-                  <button id="nextbutton" onclick="question6()">Next</button>
+                  <div id="nextbutton" class="button" onclick="question6()">Next</div>
               </th>
           </tr>
           <tr>
@@ -207,52 +198,51 @@ function question2() {
   </table>
 </div>
 `;
+setListeners();
   }
 
   function question6() {
     document.getElementById("question").innerHTML = `
-    <div id="question" class="q6">
-    <p class="text">Et le babyfoot ???</p>
+    <div class="formSection q6">
+    <p class="text boldP">Et le babyfoot ???</p>
     <table>
         <form action="">
             <tr>
 
                 <th>
-                    <img src="../image/troll_babyfoot.png" alt="André De Soussa">
+                    <img id="img1" class="choice" src="../image/troll_babyfoot.png" alt="André De Soussa">
                 </th>
                 <th>
-                    <img src="../image/renoi_babyfoot.jpg" alt="Julien Pavon">
+                    <img id="img2" class="choice" src="../image/renoi_babyfoot.jpg" alt="Julien Pavon">
                 </th>
                 <th>
-                    <img src="../image/babyfoot_aveugle.jpg" alt="Guillaume Warckol">
+                    <img id="img3" class="choice" src="../image/babyfoot_aveugle.jpg" alt="Guillaume Warckol">
                 </th>
             </tr>
             <tr>
                 <th>
-                    <input type="radio" id="contactChoice1" name="contact" value="André De Sousa">
+                
                     <label for="contactChoice1">Quel babyfoot? </label>
                 </th>
 
                 <th>
-                    <input type="radio" id="contactChoice2" name="contact" value="Julien Pavon">
                     <label for="contactChoice2">Il est en route</label>
                 </th>
 
                 <th>
-                    <input type="radio" id="contactChoice3" name="contact" value="Guillaume Warckol">
                     <label for="contactChoice3">Je ne vois pas de quoi vous parlez ?</label>
                 </th>
             </tr>
             <tr>
                 <th></th>
                 <th>
-                    <button id="nextbutton" onclick="question7()">Next</button>
+                    <div id="nextbutton" class="button" onclick="question7()">Next</div>
                 </th>
             </tr>
             <tr>
                     <th>
                         <p>
-                            <b> HELP : </b> Soit franc envers toi-même...</p>
+                            <b> HELP : </b> Sois franche envers toi-même...</p>
                     </th>
                 </tr>
 
@@ -261,52 +251,50 @@ function question2() {
 </div>
 
     `;
+    setListeners();
   }
 
   function question7() {
     document.getElementById("question").innerHTML = `
-    <div id="question" class="q7">
-        <p class="text">Qu'a tu préféré cette année ?</p>
+    <div class="formSection q7">
+        <p class="text boldP">Qu'as-tu préféré cette année ?</p>
         <table>
             <form action="">
                 <tr>
 
                     <th>
-                        <img src="../image/coding_hero.png" alt="André De Soussa">
+                        <img id="img1" class="choice" src="../image/coding_hero.png" alt="André De Soussa">
                     </th>
                     <th>
-                        <img src="../image/coding_bureau.jpg" alt="Julien Pavon">
+                        <img id="img2" class="choice" src="../image/coding_bureau.jpg" alt="Julien Pavon">
                     </th>
                     <th>
-                        <img src="../image/autre_chos.jpg" alt="Guillaume Warckol">
+                        <img id="img3" class="choice" src="../image/autre_chos.jpg" alt="Guillaume Warckol">
                     </th>
                 </tr>
                 <tr>
                     <th>
-                        <input type="radio" id="contactChoice1" name="contact" value="André De Sousa">
-                        <label for="contactChoice1">Les élèves, ont est tous adorables... </label>
+                        <label for="contactChoice1">Les élèves, on est tous adorables... </label>
                     </th>
 
                     <th>
-                        <input type="radio" id="contactChoice2" name="contact" value="Julien Pavon">
                         <label for="contactChoice2">Les magnifiques locaux qui donnent envie de venir</label>
                     </th>
 
                     <th>
-                        <input type="radio" id="contactChoice3" name="contact" value="Guillaume Warckol">
-                        <label for="contactChoice3">Autre chose que tu souhaite nous partager ? </label>
+                        <label for="contactChoice3">Autre chose que tu souhaites nous partager ? </label>
                     </th>
                 </tr>
                 <tr>
                     <th></th>
                     <th>
-                        <button id="nextbutton" onclick="End()">Fin...</button>
+                        <div id="nextbutton" class="button" onclick="End()">Fin...</div>
                     </th>
                 </tr>
                 <tr>
                     <th>
                         <p>
-                            <b> HELP : </b> Ta pas besoin d'aide pour celle la</p>
+                            <b> HELP : </b> T'as pas besoin d'aide pour celle-là</p>
                     </th>
                 </tr>
 
@@ -315,19 +303,20 @@ function question2() {
     </div>
 
     `;
+    setListeners();
   }
 
   function surprise() {
     document.getElementById("surprise").innerHTML = `
-    <div id="question" class="q7">
-    <p class="text">La surprise !!!</p>
+    <div class="formSection q7">
+    <p class="text boldP">La surprise !!!</p>
     <audio id="audioPlayer">
         <source src="hype_home.ogg">
         <source src="../music/Karaoké_Hugues_Aufray_Adieu_monsieur_le_professeur.mp3">
     </audio>
     
-    <button class="control" onclick="play('audioPlayer', this)">Play</button>
-    <button class="control" onclick="resume('audioPlayer')">Stop</button>
+    <div class="control button" onclick="play('audioPlayer', this)">Play</div>
+    <div class="control button" onclick="resume('audioPlayer')">Stop</div>
 <script type="text/javascript" src="../js/quizz.js"></script>
 </div>
 
@@ -336,9 +325,9 @@ function question2() {
 
 function End() {
     document.getElementById("question").innerHTML = `
-    <div id="question" class="q7">
-    <p class="text">Voici ta surprise, clic pour la découvrir !!!</p>
-    <button id="nextbutton" onclick="surprise()">Cadeau</button>
+    <div class="formSection q7">
+    <p class="text boldP">Voici ta surprise, clique pour la découvrir !!!</p>
+    <div id="nextbutton" class="button" onclick="surprise()">Cadeau</div>
     <div id="surprise">
 
 
@@ -365,3 +354,43 @@ function resume(idPlayer) {
     player.currentTime = 0;
     player.pause();
 }
+
+function activeImg(el){
+    el.className = "choice active";
+    if(el.id == "img1"){
+        document.getElementById("img3").className = "choice";
+        document.getElementById("img2").className = "choice";
+    }
+    else if(el.id == "img2"){
+        document.getElementById("img1").className = "choice";
+        document.getElementById("img3").className = "choice";
+    }
+    else {
+        document.getElementById("img1").className = "choice";
+        document.getElementById("img2").className = "choice";
+    }
+}
+
+function setListeners(){
+    document.getElementById("img1").addEventListener('click', function(){
+        document.getElementById("img1").className = "choice activeChoice";
+        document.getElementById("img2").className = "choice";
+        document.getElementById("img3").className = "choice";
+    });
+    
+    document.getElementById("img2").addEventListener('click', function(){
+        document.getElementById("img2").className = "choice activeChoice";
+        document.getElementById("img1").className = "choice";
+        document.getElementById("img3").className = "choice";
+    });
+    
+    document.getElementById("img3").addEventListener('click', function(){
+        document.getElementById("img3").className = "choice activeChoice";
+        document.getElementById("img2").className = "choice";
+        document.getElementById("img1").className = "choice";
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    setListeners();
+});
